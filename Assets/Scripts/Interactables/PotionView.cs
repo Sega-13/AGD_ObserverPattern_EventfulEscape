c459408886ae229e4d36708b6f2252ecb522f8d2
Sega-13 <segapatidar@gmail.com>
2024-07-29 16:10:38 +0530

    Implemented OnPotionDrinkEvent

--- Assets/Scripts/Interactables/PotionView.cs
+++ Assets/Scripts/Interactables/PotionView.cs
@@ -4,7 +4,13 @@ public class PotionView : MonoBehaviour, IInteractable
 {
     [SerializeField] SoundType soundType;
     private int potionEffect = 20;
-
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<PlayerView>() != null)
+        {
+            EventService.Instance.OnPotionDrink.InvokeEvent(potionEffect);
+        }
+    }
     public void Interact()
     {
         GameService.Instance.GetInstructionView().HideInstruction();
